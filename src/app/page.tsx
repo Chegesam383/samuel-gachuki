6e039722da99ab76d6070b8cc274d7858b58500d
Chegesam383 <chegegachuki@gmail.com>
2025-02-05 02:00:18 -0500

    removed unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import * as React from "react";
-import { tools } from "../lib/constants";
-import { Card } from "@/components/ui/card";
-
-import { ArrowRight, MapPin } from "lucide-react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import Hero from "@/components/hero";
 import ServicesSection from "@/components/services";
 import ProjectsSection from "@/components/projects";
