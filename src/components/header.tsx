08d69754662ec378ac9646c49028bba19820916f
Chegesam383 <chegegachuki@gmail.com>
2025-02-21 10:21:01 -0500

    added hero animations fixd header padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,12 +15,12 @@ import { TextEffect } from "./ui/text-animation";
 
 const Header = () => {
   return (
-    <header className="">
+    <header>
       <nav className="border-foreground-muted fixed top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="flex justify-between py-2 max-w-6xl items-center container mx-auto">
+        <div className="flex justify-between p-4 max-w-6xl items-center container mx-auto">
           <Link
             href="/"
-            className={`ml-4 text-xl md:text-2xl font-bold gradient ${mynerve.className}`}
+            className={`text-xl md:text-2xl font-bold gradient ${mynerve.className}`}
           >
             <TextEffect per="char" preset="fade">
               Samuel Gachuki
